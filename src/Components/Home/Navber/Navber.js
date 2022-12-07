fe903a01198afbaf3af47dca86957b85d278f016
Nazmush Shakib <shakibgithub944>
2022-12-08 01:08:43 +0600

    navber complete"

--- src/Components/Home/Navber/Navber.js
+++ src/Components/Home/Navber/Navber.js
@@ -7,18 +7,18 @@ const Navber = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <h1 className="navbar-brand fs-3 mx-5"><span className='text-success'>ATG.</span>W<span ><img src={logo} style={{height:'20px'}} alt="" /></span>RLD</h1>
+                    <h1 className="navbar-brand fs-3 mx-5"><span className='text-success'>ATG.</span>W<span ><img src={logo} style={{ height: '20px' }} alt="" /></span>RLD</h1>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />  
+                            <input className="form-control me-2" style={{ width: '360px' }} type="search" placeholder="Search" aria-label="Search" />
                         </ul>
-                        <form className="d-flex">
-                        
-                            <button className="btn btn-outline-success" >Login</button>
-                        </form>
+
+                        <div className="">
+                            <p className='fw-bold'>Create account <span className='text-primary'>It's free</span></p>
+                        </div>
                     </div>
                 </div>
             </nav>
